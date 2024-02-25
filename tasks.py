0240279b2a7da240ccbb074812c46da45116fd51
Dmitri <damirakyan@gmail.com>
2024-02-25 17:49:54 -0500

    dependency tweaks

--- tasks.py
+++ tasks.py
@@ -3,6 +3,9 @@ import requests
 import supabase
 from celery import Celery
 from celery.utils.log import get_task_logger
+from supabase import create_client, Client
+from uplink import Consumer, post, Body, json, Path
+
 
 app = Celery('tasks', broker=os.getenv("CELERY_BROKER_URL"))
 logger = get_task_logger(__name__)
