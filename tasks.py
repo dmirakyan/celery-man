bb8a06357e4ebfe401f6eca4f3e9cbc342535e60
Dmitri <damirakyan@gmail.com>
2024-02-25 19:33:06 -0500

    Testing;

--- tasks.py
+++ tasks.py
@@ -87,9 +87,10 @@ def push_outputs(email,tune_id):
     prompts_json = requests.get(f'https://api.astria.ai/tunes/{tune_id}/prompts', headers=astria_headers).json()
     astria_images = []
 
-    for item in prompts_json[0:1]:
+    for item in prompts_json:
         if 'images' in item:
             astria_images.extend(item['images']) 
+
     for link in astria_images:
         filename = link[-14:]
         response = requests.get(link)
